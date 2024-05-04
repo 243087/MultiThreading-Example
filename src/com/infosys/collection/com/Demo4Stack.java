c061ceead59db1de5b1c84c81002d1f89847318c
Rahul <rahulkumar243k@gmail.com>
2024-05-04 21:13:26 +0530

    third time checkin

--- src/com/infosys/collection/com/Demo4Stack.java
+++ src/com/infosys/collection/com/Demo4Stack.java
@@ -5,6 +5,6 @@ public class Demo4Stack {
 	public static void main(String[] args) {
 		System.out.println("Check");
 		for(int i=0;i<10;i++)
-			System.out.println("Hello Rahul");
+			System.out.println("Hello Rahul");  //all good
 	}
 }
